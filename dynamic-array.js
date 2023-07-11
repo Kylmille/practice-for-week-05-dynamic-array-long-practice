57b66294fe08a03735e0925c6938cba6e254cceb
Kyle <Kylesiphone625@gmail.com>
2023-07-11 19:46:15 -0400

    new file:   .gitignore 	modified:   dynamic-array.js 	modified:   package-lock.json

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,46 +1,128 @@
+
 class DynamicArray {
+  constructor(capacity = 4) {
+    // Set the initial capacity for the dynamic array
+    this.capacity = capacity;
 
-  constructor(defaultSize = 4) {
+    // Initialize the length of the dynamic array to 0
+    this.length = 0;
 
-    // Your code here
+    // Create a new array with the specified capacity
+    this.data = new Array(capacity);
   }
 
   read(index) {
-
-    // Your code here
+    // Retrieve the element at the given index from the array
+    return this.data[index];
   }
 
-  push(val) {
+  push(value) {
+    // Check if the array is already at its capacity
+    if (this.length === this.capacity) {
+      // If it is, resize the array to increase its capacity
+      this.resize();
+    }
 
-    // Your code here
+    // Add the value to the end of the array
+    this.data[this.length] = value;
+
+    // Increment the length of the array
+    this.length++;
   }
-
 
   pop() {
+    // Check if the array is empty
+    if (this.length === 0) {
+      // If it is, return undefined
+      return undefined;
+    }
 
-    // Your code here
+    // Retrieve the last value in the array
+    const lastValue = this.data[this.length - 1];
+
+    // Set the last value in the array to undefined
+    this.data[this.length - 1] = undefined;
+
+    // Decrement the length of the array
+    this.length--;
+
+    // Return the last value that was removed
+    return lastValue;
+  }
+
+  unshift(value) {
+    // Check if the array is already at its capacity
+    if (this.length === this.capacity) {
+      // If it is, resize the array to increase its capacity
+      this.resize();
+    }
+
+    // Shift all elements to the right by one position
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+
+    // Insert the new value at the beginning of the array
+    this.data[0] = value;
+
+    // Increment the length of the array
+    this.length++;
   }
 
   shift() {
+    // Check if the array is empty
+    if (this.length === 0) {
+      // If it is, return undefined
+      return undefined;
+    }
 
-    // Your code here
+    // Retrieve the first value in the array
+    const firstValue = this.data[0];
+
+    // Shift all elements to the left by one position
+    for (let i = 0; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    // Set the last value in the array to undefined
+    this.data[this.length - 1] = undefined;
+
+    // Decrement the length of the array
+    this.length--;
+
+    // Return the first value that was removed
+    return firstValue;
   }
 
-  unshift(val) {
+  indexOf(value) {
+    // Iterate over each element in the array
+    for (let i = 0; i < this.length; i++) {
+      // Check if the current element matches the given value
+      if (this.data[i] === value) {
+        // If it does, return the index of the element
+        return i;
+      }
+    }
 
-    // Your code here
-  }
-
-  indexOf(val) {
-
-    // Your code here
+    // If the value is not found, return -1
+    return -1;
   }
 
   resize() {
+    // Double the current capacity of the array
+    this.capacity *= 2;
 
-    // Your code here
+    // Create a new array with the updated capacity
+    const newData = new Array(this.capacity);
+
+    // Copy all elements from the old array to the new array
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i];
+    }
+
+    // Replace the old array with the new array
+    this.data = newData;
   }
-
 }
 
 
